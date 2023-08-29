f539b3b6759b73ea0a20455da09976f704e8eda3
Niirin <laynilin@gmail.com>
2023-08-29 13:51:31 +0700

    Finished coding Task edit function and added two more pictures.

--- src/components/TaskDisplay.jsx
+++ src/components/TaskDisplay.jsx
@@ -1,31 +1,47 @@
 import { useState } from "react";
 import Modal from "react-overlays/Modal";
+import TaskEdit from "./TaskEdit";
 
-function TaskDisplay({task, onDelete}) {
+function TaskDisplay({task, onDelete, onEdit}) {
     const [showModal, setshowModal] = useState(false);
 
     const handleOff = () => setshowModal(false);
 
     const renderBackdrop = (props) => <div className="backdrop" {...props} />;
+
     const handleDeleteClick= () =>{
         handleOff();
         onDelete(task.id);
     };
+    const [showEdit, setShowEdit] = useState(false);
 
+    const [showDisplayIcons, setShowDisplayIcons]= useState(true);
 
+    const handleSubmit = (id, newTitle) => {
+        setShowEdit(false);
+        onEdit(id, newTitle);
+        setShowDisplayIcons(true);
+    }
+
+    let display = <span>{task.title}</span>;
+    if (showEdit){
+        display = <TaskEdit onSubmit={handleSubmit} task={task} />;
+    }
     
-    return <section className='container'>
-            <div className='taskList'>
+    return <section className="container">
+            <div className="task-list">
             <ul>
-                <li className='taskDis'>
-                    <div className='taskCheck'>
+                <li className="task-dis">
+                    <div className="task-check">
                         <input type="checkbox" />
-                        <span>{task.title}</span>
+                        <span>{display}</span>
                     </div>
+                    {showDisplayIcons &&(
                         <div className="icons">
-                            <img  src='./src/assets/edit.svg' alt="edit icon" />
-                            <img onClick={() => setshowModal(true)} src='./src/assets/delete_icon.png' alt="delete icon" />
+                            <img onClick={handleEditClick} src="./src/assets/edit.svg" alt="edit" />
+                            <img onClick={() => setshowModal(true)} src="./src/assets/delete_icon.png" alt="delete" />
                         </div>
+                    )}                        
                 </li>
             </ul>
             </div>
